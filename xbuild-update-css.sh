b75f63f40b3478bf85aa5059a154d941f25bce24
HaoYin <arno@idorp.com>
2018-06-25 10:55:00 +0800

    [NOVERIFY][IdorpAngularJS][T][FIX #1]添加测试发布脚本， 启用 scss 支持2

--- xbuild-update-css.sh
+++ xbuild-update-css.sh
@@ -2,5 +2,7 @@
 
 # 启用 scss 模式开发中， 通过 main.scss 更新 main.css 文件 
 ./node_modules/node-sass/bin/node-sass src/client/scss/main.scss src/client/css/main.css   
+# ./node_modules/node-sass/bin/node-sass src/client/scss/main.scss src/client/css/main.css --source-map-root src/client/css/map --source-map-embed true
+# ./node_modules/node-sass/bin/node-sass src/client/scss/main.scss src/client/css/main.css --source-map-root src/client/css/map --source-map-embed false
 
 
