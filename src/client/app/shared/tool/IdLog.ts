988620485c5d5a5d64af7c007dcb324d778c0196
walker <walker@idorp.com>
2018-11-15 16:17:14 +0800

    [NOVERIFY]重构

---
+++ src/client/app/shared/tool/IdLog.ts
@@ -0,0 +1,49 @@
+export class IdLog {
+    private static LOG_DEBUG = true;
+    private static LOG_WARN = true;
+    private static LOG_ERROR = true;
+    public static log(message?: any, ...optionalParams: any[]) {
+        if (this.LOG_DEBUG) {
+            if (optionalParams.length > 0) {
+                console.log(message, optionalParams);
+            } else {
+                console.log(message);
+            }
+        }
+    }
+
+    public static debug(message?: any, ...optionalParams: any[]) {
+        if (this.LOG_DEBUG) {
+            if (optionalParams.length > 0) {
+                console.log(message, optionalParams);
+            } else {
+                console.log(message);
+            }
+        }
+    }
+
+    public static warn(message?: any, ...optionalParams: any[]) {
+        if (this.LOG_WARN) {
+            if (optionalParams.length > 0) {
+                console.warn(message, optionalParams);
+            } else {
+                console.warn(message);
+            }
+        }
+    }
+
+    /**
+     * 错误日志，默认输出
+     * @param message 错误提示
+     * @param optionalParams 参数
+     */
+    public static error(message?: any, ...optionalParams: any[]) {
+        if (this.LOG_ERROR) {
+            if (optionalParams.length > 0) {
+                console.error(message, optionalParams);
+            } else {
+                console.error(message);
+            }
+        }
+    }
+}
